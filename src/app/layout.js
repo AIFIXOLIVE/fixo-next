5f226a2b61dcf8c699bcabba37fb9efbf337b6b9
AIFIXOLIVE <dev.fixo.live@gmail.com>
2025-03-26 16:13:21 -0100

    Add global menu in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,25 @@ import './globals.css';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+      <header>
+          <nav className="nav">
+            <h1 className="logo-text">AI.FIXØ.LIVE</h1>
+            <button className="menu-toggle" aria-label="Open menu">
+              <span className="bar"></span>
+              <span className="bar"></span>
+              <span className="bar"></span>
+            </button>
+            <ul className="nav-links">
+              <li><a href="#tools">⚙️ AI TOOLS</a></li>
+              <li><a href="#hot">📊 HOT AI</a></li>
+              <li><a href="#e-commerce">📢 AI NEWS</a></li>
+              <li><a href="#ai-chatbot">🤖 AI CHATBOT</a></li>
+            </ul>
+          </nav>
+        </header>  
+        {children}
+        </body>
     </html>
   );
 }
